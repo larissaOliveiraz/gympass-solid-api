69b0583c049937a8927f90e1daa2a334e9a14fbe
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-08-17 19:54:43 -0300

    feat: create route post /users

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
+import { registerUser } from "./controllers/register-controller";
+import { appRoutes } from "./routes";
 
 export const app = fastify();
+
+app.register(appRoutes);

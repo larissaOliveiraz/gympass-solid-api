3e80bebe3250d358920bcc45b6baf783064c3cdd
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-08-29 19:15:59 -0300

    feat: add check in history with pagination

---
+++ src/services/fetch-user-check-in-history-service.ts
@@ -0,0 +1,27 @@
+import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { CheckIn } from "@prisma/client";
+
+interface FetchUserCheckInHistoryServiceRequest {
+   userId: string;
+   page: number;
+}
+
+interface FetchUserCheckInHistoryServiceResponse {
+   checkIns: CheckIn[];
+}
+
+export class FetchUserCheckInHistoryService {
+   constructor(private checkInsRepository: CheckInsRepository) {}
+
+   async execute({
+      userId,
+      page,
+   }: FetchUserCheckInHistoryServiceRequest): Promise<FetchUserCheckInHistoryServiceResponse> {
+      const checkIns = await this.checkInsRepository.findManyByUserId(
+         userId,
+         page
+      );
+
+      return { checkIns };
+   }
+}

41fa6e1aa189cddbcaee8a10d24274574c6dd119
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-08-31 12:45:51 -0300

    feat: craete prisma repository implementation

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,16 +3,14 @@ import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
-   async findById(
-      id: string
-   ): Promise<{
-      id: string;
-      name: string;
-      email: string;
-      password_hash: string;
-      created_at: Date;
-   } | null> {
-      throw new Error("Method not implemented.");
+   async findById(id: string) {
+      const user = await prisma.user.findUnique({
+         where: {
+            id,
+         },
+      });
+
+      return user;
    }
 
    async findByEmail(email: string) {

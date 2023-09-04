3a692e6e6ce458271d1ff5a172ed8f76aa481d82
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-09-04 16:16:11 -0300

    feat: implement refresh token controller and tests

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { registerUser } from "./register";
 import { authenticateUser } from "./authentication";
 import { profile } from "./profile";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function userRoutes(app: FastifyInstance) {
    app.post("/users", registerUser);
    app.post("/sessions", authenticateUser);
+
+   app.patch("/token/refresh", refresh);
 
    // Authenticated
    app.get("/me", { onRequest: [verifyJWT] }, profile);

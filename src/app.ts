3d09cf237a7ebca46be04e2d5423c20b536baddf
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-09-04 15:52:50 -0300

    feat: add refreshToken cookie

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import { fastifyJwt } from "@fastify/jwt";
+import cookie from "@fastify/cookie";
 import { gymRoutes } from "./http/controllers/gyms/routes";
 import { userRoutes } from "./http/controllers/users/routes";
 import { checkInRoutes } from "./http/controllers/check-ins/routes";
@@ -10,7 +11,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
    secret: env.JWT_SECRET,
+   cookie: {
+      cookieName: "refreshToken",
+      signed: false,
+   },
+   sign: {
+      expiresIn: "10m",
+   },
 });
+
+app.register(cookie);
 
 app.register(userRoutes);
 app.register(gymRoutes);
